640d063c66ee31292d6b60a9aad109640b7e982b
Yash Dalia <20it020@charusat.edu.in>
2022-09-14 12:12:22 +0530

    commit

--- src/main/java/in/ac/charusat/studentmgmtsystem/repository/StudentRepository.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/repository/StudentRepository.java
@@ -3,5 +3,6 @@ package in.ac.charusat.studentmgmtsystem.repository;
 import in.ac.charusat.studentmgmtsystem.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
